4c17c5662d308bcf8bfed5225a6efec08112f7b7
akshayjaiswal01 <akshayjaiswal608@gmail.com>
2025-12-16 19:14:42 +0530

    C# Basics

---
+++ Solution_01.sln/MyApp_01/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp_01
+{
+    internal class Class1
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public Boolean checkPrime(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+
+           for(int i= 2; i < num; i++)
+           {
+                if(num % i == 0)
+                {
+                    return false;
+                }
+           }
+
+            return true;
+        }
+
+       public 
+
+    }
+}
---
+++ Solution_01.sln/MyApp_01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MyApp_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+
+            #region Hello
+            // obj1.sayHello();
+            #endregion
+
+            #region Check Prime
+            //Console.WriteLine("Enter Number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //Boolean status = obj1.checkPrime(num);
+            //if(status)
+            //{
+            //    Console.WriteLine("Number {0} is prime number", num);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number {0} is not prime number", num);
+            //}
+            #endregion
+
+
+        }
+    }
+}
